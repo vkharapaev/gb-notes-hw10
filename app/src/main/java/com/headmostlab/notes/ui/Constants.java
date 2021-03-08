2403cc0c077481755b34fff60d2289c654f009a1
Vladimir Kharapaev <vkharapaev@gmail.com>
2021-03-09 01:57:09 +0500

    Refactoring

--- app/src/main/java/com/headmostlab/notes/ui/Constants.java
+++ app/src/main/java/com/headmostlab/notes/ui/Constants.java
@@ -1,6 +1,6 @@
 package com.headmostlab.notes.ui;
 
-public class Constants {
-    public static final String FRAGMENT_RESULT_DESELECT_NOTE = "DESELECT_NOTE";
-    public static final String COLLECTION_NOTES = "notes";
+public interface Constants {
+    String FRAGMENT_RESULT_DESELECT_NOTE = "DESELECT_NOTE";
+    String COLLECTION_NOTES = "notes";
 }
